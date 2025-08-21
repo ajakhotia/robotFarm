8237f28c568295573a7ed8da9c0ebf2155ce2040
Anurag Jakhotia <5448528+ajakhotia@users.noreply.github.com>
2025-08-21 13:20:37 +0200

    fix(cmake): openmp ceres solver build

--- cmake/CeresSolverExternalProject.cmake
+++ cmake/CeresSolverExternalProject.cmake
@@ -11,6 +11,24 @@ include(${CMAKE_CURRENT_LIST_DIR}/SuiteSparseExternalProject.cmake)
 
 option(ROBOT_FARM_SKIP_CeresSolverExternalProject "Forcefully skip Ceres Solver" OFF)
 
+set(OMP_FLAG_C   "")
+set(OMP_FLAG_CXX "")
+set(OMP_LINK     "")
+
+if(CMAKE_C_COMPILER_ID MATCHES "AppleClang" OR CMAKE_CXX_COMPILER_ID MATCHES "AppleClang")
+    set(OMP_FLAG_C   "-Xpreprocessor -fopenmp")
+    set(OMP_FLAG_CXX "-Xpreprocessor -fopenmp")
+    set(OMP_LINK     "-lomp")
+elseif(CMAKE_C_COMPILER_ID MATCHES "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+    set(OMP_FLAG_C   "-fopenmp")
+    set(OMP_FLAG_CXX "-fopenmp")
+    set(OMP_LINK     "-fopenmp")
+elseif(CMAKE_C_COMPILER_ID MATCHES "Clang" OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+    message(STATUS "Don't yet know what to do for Open MP with Clang")
+else()
+    message(WARNING "Unknown compiler family; not adding OpenMP flags for Ceres external build.")
+endif()
+
 if(ROBOT_FARM_SKIP_CeresSolverExternalProject)
     add_custom_target(CeresSolverExternalProject)
 else()
@@ -25,7 +43,12 @@ else()
         PREFIX ${CMAKE_CURRENT_BINARY_DIR}/ceressolver
         URL ${ROBOT_FARM_CERES_SOLVER_URL}
         DOWNLOAD_NO_PROGRESS ON
-        CMAKE_ARGS ${ROBOT_FARM_FORWARDED_CMAKE_ARGS})
+        CMAKE_ARGS
+          ${ROBOT_FARM_FORWARDED_CMAKE_ARGS}
+          -DCMAKE_C_FLAGS=${OMP_FLAG_C}
+          -DCMAKE_CXX_FLAGS=${OMP_FLAG_CXX}
+          -DCMAKE_EXE_LINKER_FLAGS=${OMP_LINK}
+          -DCMAKE_SHARED_LINKER_FLAGS=${OMP_LINK})
 endif()
 
 add_dependencies(CeresSolverExternalProject
